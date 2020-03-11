32b311f573d8083936b2e3e0ca2be4ca82926811
ambernina <rob.ambernina@gmail.com>
2020-03-10 22:19:05 -0600

    added server code

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const Workout = require("./models/workoutmodel");
+require("./public/api");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+	useNewUrlParser: true,
+	useFindAndModify: false
+});
+
+// routes
+app.post("/submit", ({ body }, res) => {
+	Workout.create(body)
+		.then(dbWorkout => {
+			res.json(dbWorkout);
+		})
+		.catch(err => {
+			res.json(err);
+		});
+});
+
+app.listen(PORT, () => {
+	console.log(`App running on port ${PORT}!`);
+});
