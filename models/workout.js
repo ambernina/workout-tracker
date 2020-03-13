57d71a5f349dd19d6896abb50444d3765ff3f3b9
ambernina <rob.ambernina@gmail.com>
2020-03-13 16:02:55 -0600

    removed all unnecessary elements from schema

---
+++ models/workout.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  exercises: {
+    type: Array,
+    default: []
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
