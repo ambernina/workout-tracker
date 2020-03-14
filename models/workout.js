cc6f827f7efaac83a5cc42e655c7c0d452c912d8
ambernina <rob.ambernina@gmail.com>
2020-03-14 16:12:31 -0600

    updated model to include more than just an empty array

--- models/workout.js
+++ models/workout.js
@@ -3,16 +3,52 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  exercises: {
-    type: Array,
-    default: []
-  },
-  date: {
+	day: {
     type: Date,
-    default: Date.now
+    default: () => new Date()
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Enter an exercise type"
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter an exercise name"
+      },
+      duration: {
+        type: Number,
+        required: "Enter an exercise duration in minutes"
+      },
+      weight: {
+        type: Number
+      },
+      reps: {
+        type: Number
+      },
+      sets: {
+        type: Number
+      },
+      distance: {
+        type: Number
+      }
+    }
+  ]
+},
+{
+  toJSON: {
+    virtuals: true
   }
 });
 
+workoutSchema.virtual("totalDuration").get(function() {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
