332d334be4a04cf7446bdc585c7cc124579ccd55
ambernina <rob.ambernina@gmail.com>
2020-03-14 16:12:59 -0600

    changed server number

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
-const PORT = process.env.PORT || 5555;
+const PORT = process.env.PORT || 3001;
 const db = require("./models/workout");
 
 const app = express();
